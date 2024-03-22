ed51609f61755fb324321a03263bcbac64cfe31a
Kyle Chard <chard@login02.expanse.sdsc.edu>
2024-03-22 13:57:42 -0700

    updating scripts

--- flower/run.sh
+++ flower/run.sh
@@ -9,7 +9,7 @@ sleep 3  # Sleep for 3s to give the server enough time to start
 
 for i in $(seq 1 $1); do
     echo "Starting client $i"
-    python client.py --partition-id 1 &
+    python client.py --partition-id 1 --model $2 --ip $3 &
 done
 
 # Enable CTRL+C to stop all background processes
