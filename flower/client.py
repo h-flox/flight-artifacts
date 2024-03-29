057facd6f68539e605555d7bd8bad680ca894ce7
Kyle Chard <chard@exp-9-56.expanse.sdsc.edu>
2024-03-29 09:33:03 -0700

    Added client.

--- flower/client.py
+++ flower/client.py
@@ -71,23 +71,24 @@ def test(net, testloader):
 
 def load_data(partition_id):
     """Load partition CIFAR10 data."""
-    fds = FederatedDataset(dataset="cifar10", partitioners={"train": 3})
-    partition = fds.load_partition(partition_id)
+    #fds = FederatedDataset(dataset="cifar10", partitioners={"train": 3})
+    #partition = fds.load_partition(partition_id)
     # Divide data on each node: 80% train, 20% test
-    partition_train_test = partition.train_test_split(test_size=0.2)
-    pytorch_transforms = Compose(
-        [ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
-    )
+    #partition_train_test = partition.train_test_split(test_size=0.2)
+    #pytorch_transforms = Compose(
+    #    [ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    #)
 
-    def apply_transforms(batch):
-        """Apply transforms to the partition from FederatedDataset."""
-        batch["img"] = [pytorch_transforms(img) for img in batch["img"]]
-        return batch
+    #def apply_transforms(batch):
+    #    """Apply transforms to the partition from FederatedDataset."""
+    #    batch["img"] = [pytorch_transforms(img) for img in batch["img"]]
+    #    return batch
 
-    partition_train_test = partition_train_test.with_transform(apply_transforms)
-    trainloader = DataLoader(partition_train_test["train"], batch_size=32, shuffle=True)
-    testloader = DataLoader(partition_train_test["test"], batch_size=32)
-    return trainloader, testloader
+    #partition_train_test = partition_train_test.with_transform(apply_transforms)
+    #trainloader = DataLoader(partition_train_test["train"], batch_size=32, shuffle=True)
+    #testloader = DataLoader(partition_train_test["test"], batch_size=32)
+    #return trainloader, testloader
+    return DataLoader([0]), DataLoader([0])
 
 
 # #############################################################################
