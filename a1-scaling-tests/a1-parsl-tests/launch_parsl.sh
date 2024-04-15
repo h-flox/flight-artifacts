94748d39156d402e9f59f9cf1080c863719da2bc
Yadu Babuji <yadudoc1729@gmail.com>
2024-04-15 15:16:15 -0500

    Trimming down launch scripts

---
+++ a1-scaling-tests/a1-parsl-tests/launch_parsl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sbatch sbatch_scripts/multinode_2_nodes.sbatch
+sbatch sbatch_scripts/multinode_3_nodes.sbatch
+sbatch sbatch_scripts/multinode_5_nodes.sbatch
+sbatch sbatch_scripts/multinode_9_nodes.sbatch
+sbatch sbatch_scripts/multinode_17_nodes.sbatch
---
+++ a1-scaling-tests/a1-parsl-tests/launch_parsl_redisconnector.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sbatch sbatch_scripts/redis_2_nodes.sbatch
+sbatch sbatch_scripts/redis_3_nodes.sbatch
+sbatch sbatch_scripts/redis_5_nodes.sbatch
+sbatch sbatch_scripts/redis_9_nodes.sbatch
+sbatch sbatch_scripts/redis_17_nodes.sbatch
